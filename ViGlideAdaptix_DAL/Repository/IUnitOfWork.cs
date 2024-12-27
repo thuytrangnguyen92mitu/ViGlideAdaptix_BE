07ed84cc5d44babd674adaebeaaf4e38656ec7a9
TriNHM <triminh0502@gmail.com>
2024-12-27 21:35:51 +0700

    [ADD] add unit of work

---
+++ ViGlideAdaptix_DAL/Repository/IUnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViGlideAdaptix_DAL.Models;
+
+namespace ViGlideAdaptix_DAL.Repository
+{
+	public interface IUnitOfWork : IDisposable
+	{
+		IGenericRepository<ShoppingCart> CartRepository { get; }
+		IGenericRepository<ShoppingCartItem> CartItemRepository { get; }
+		IGenericRepository<Product> ProductRepository { get; }
+		IGenericRepository<Customer> CustomerRepository { get; }
+		IGenericRepository<PaymentMethod> PaymentMethodRepository { get; }
+		IGenericRepository<Order> OrderRepository { get; }
+		IGenericRepository<OrderDetail> OrderDetailRepository { get; }
+
+
+		Task BeginTransactionAsync();
+		Task CommitTransactionAsync();
+		Task RollbackTransactionAsync();
+		Task SaveChangesAsync();
+	}
+}
---
+++ ViGlideAdaptix_DAL/Repository/UnitOfWork.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViGlideAdaptix_DAL.Models;
+
+namespace ViGlideAdaptix_DAL.Repository
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		private readonly ViGlideAdaptixContext _context;
+		private IDbContextTransaction? _transaction;
+
+		public IGenericRepository<ShoppingCart> CartRepository { get; }
+		public IGenericRepository<ShoppingCartItem> CartItemRepository { get; }
+		public IGenericRepository<Product> ProductRepository { get; }
+		public IGenericRepository<Customer> CustomerRepository { get; }
+
+		public IGenericRepository<PaymentMethod> PaymentMethodRepository { get; }
+
+		public IGenericRepository<Order> OrderRepository { get; }
+		public IGenericRepository<OrderDetail> OrderDetailRepository { get; }
+
+		public UnitOfWork(ViGlideAdaptixContext context)
+		{
+			_context = context;
+			CartRepository = new GenericRepository<ShoppingCart>(_context);
+			CartItemRepository = new GenericRepository<ShoppingCartItem>(_context);
+			ProductRepository = new GenericRepository<Product>(_context);
+			CustomerRepository = new GenericRepository<Customer>(_context);
+			PaymentMethodRepository = new GenericRepository<PaymentMethod>(_context);
+			OrderRepository = new GenericRepository<Order>(_context);
+			OrderDetailRepository = new GenericRepository<OrderDetail>(_context);
+		}
+		public async Task BeginTransactionAsync()
+		{
+			_transaction = await _context.Database.BeginTransactionAsync();
+		}
+
+		public async Task CommitTransactionAsync()
+		{
+			if (_transaction != null)
+			{
+				await _transaction.CommitAsync();
+				await _transaction.DisposeAsync();
+				_transaction = null;
+			}
+		}
+		public async Task RollbackTransactionAsync()
+		{
+			if (_transaction != null)
+			{
+				await _transaction.RollbackAsync();
+				await _transaction.DisposeAsync();
+				_transaction = null;
+			}
+		}
+		public async Task SaveChangesAsync()
+		{
+			await _context.SaveChangesAsync();
+		}
+
+		private bool _disposed = false;
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!_disposed)
+			{
+				if (disposing)
+				{
+					_context.Dispose();
+				}
+				_disposed = true;
+			}
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+	}
+}
