defaf68b2dd251cae47dfe4de094c8ec5947f172
TriNHM <triminh0502@gmail.com>
2024-12-31 15:50:25 +0700

    [ADD] add RatingRequestDTO

---
+++ ViGlideAdaptix_BLL/DTO/RatingRequestDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViGlideAdaptix_BLL.DTO
+{
+	public class RatingRequestDTO
+	{
+		public int OrderItemId { get; set; }
+
+		public int Score { get; set; }
+
+		public string? Comment { get; set; }
+
+		public DateTime RatingDate { get; set; }
+	}
+}
