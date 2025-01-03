4743f24de388a56e0ea00a682447f7ec59c4c5cb
TriNHM <triminh0502@gmail.com>
2025-01-03 21:29:10 +0700

    [ADD] add new CustomerInforResponseDTO

---
+++ ViGlideAdaptix_BLL/DTO/CustomerInforResponseDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViGlideAdaptix_DAL.Models;
+
+namespace ViGlideAdaptix_BLL.DTO
+{
+    public class CustomerInforResponseDTO
+    {
+
+        public string CustomerName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public string Address { get; set; } = null!;
+
+        public string PhoneNumber { get; set; } = null!;
+
+    }
+}
