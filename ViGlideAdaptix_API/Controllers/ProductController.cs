9e02de12ec6e96076d6f1b232b2e916bdbe42ecc
TriNHM <triminh0502@gmail.com>
2025-01-05 23:36:20 +0700

    [ADD] add api handle image

--- ViGlideAdaptix_API/Controllers/ProductController.cs
+++ ViGlideAdaptix_API/Controllers/ProductController.cs
@@ -6,44 +6,71 @@ using ViGlideAdaptix_BLL.Service.ProductService;
 
 namespace ViGlideAdaptix_API.Controllers
 {
-	[Route("api/product")]
-	[ApiController]
-	public class ProductController : ControllerBase
-	{
-		private readonly IProductServices _productServices;
-		public ProductController(IProductServices productServices)
-		{
-			_productServices = productServices;
-		}
+    [Route("api/product")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductServices _productServices;
+        private readonly IWebHostEnvironment _env;
+        public ProductController(IProductServices productServices, IWebHostEnvironment env)
+        {
+            _productServices = productServices;
+            _env = env;
+        }
 
-		/// <summary>
-		/// Get all product
-		/// QueryObjectDTO
-		/// Return list of data + total Record 
-		/// </summary>
-		[HttpGet]
-		public async Task<IActionResult> GetAllProduct([FromQuery] QueryObjectDTO request)
-		{
-			if (!ModelState.IsValid)
-				return BadRequest(ModelState);
+        /// <summary>
+        /// Get all product
+        /// QueryObjectDTO
+        /// Return list of data + total Record 
+        /// </summary>
+        [HttpGet]
+        public async Task<IActionResult> GetAllProduct([FromQuery] QueryObjectDTO request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
-			var result = await _productServices.GetAllProductAsync(request);
-			return Ok(result);
-		}
+            var result = await _productServices.GetAllProductAsync(request);
+            return Ok(result);
+        }
 
-		/// <summary>
-		/// Get all category list
-		/// </summary>
-		[HttpGet("category")]
-		public async Task<IActionResult> GetAllCategory()
-		{
-			var result = await _productServices.GetCateList();
-			return Ok(result);
-		}
+        [HttpGet("images")]
+        public IActionResult GetImages(string imageName)
+        {
+            //get path to the image requested
+            var imagePath = Path.Combine(_env.ContentRootPath, "uploads", imageName);
 
-		/// <summary>
-		/// Get all brand list
-		/// </summary>
+            if (!System.IO.File.Exists(imagePath))
+            {
+                return NoContent();
+            }
+
+            byte[] fileBytes;
+            using (var ms = new MemoryStream())
+            {
+                using (var defaultUserImg = new FileStream(imagePath, FileMode.Open))
+                {
+                    defaultUserImg.CopyTo(ms);
+                }
+                fileBytes = ms.ToArray();
+            }
+
+            string base64String = Convert.ToBase64String(fileBytes);
+            return Ok(base64String);
+        }
+
+        /// <summary>
+        /// Get all category list
+        /// </summary>
+        [HttpGet("category")]
+        public async Task<IActionResult> GetAllCategory()
+        {
+            var result = await _productServices.GetCateList();
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Get all brand list
+        /// </summary>
         [HttpGet("brand")]
         public async Task<IActionResult> GetAllBrand()
         {
@@ -56,30 +83,49 @@ namespace ViGlideAdaptix_API.Controllers
         /// productId
         /// </summary>
         [HttpGet("{productId}")]
-		public async Task<IActionResult> GetDetailOfProduct(int productId)
-		{
-			if (!ModelState.IsValid)
-				return BadRequest(ModelState);
+        public async Task<IActionResult> GetDetailOfProduct(int productId)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
-			var foundProduct = await _productServices.GetProductDetail(productId);
-			return Ok(foundProduct);
-		}
+            var foundProduct = await _productServices.GetProductDetail(productId);
+            return Ok(foundProduct);
+        }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		[HttpPost("add")]
-        [Authorize(Roles = "mod")]
-        public async Task<IActionResult> ModCreateProduct([FromBody] CreateProductResquestDTO request)
-		{
-			if (!ModelState.IsValid)
-				return BadRequest(ModelState);
+        /// <summary>
+        /// 
+        /// </summary>
+        [HttpPost("add")]
+        //[Authorize(Roles = "mod")]
+        public async Task<IActionResult> ModCreateProduct([FromForm] CreateProductResquestDTO request, [FromForm] IFormFile? productImage)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
-			var (isCreated, message) = await _productServices.ModCreateProduct(request);
-			if (isCreated)
-				return Ok(message);
+            if (productImage == null || productImage.Length == 0)
+            {
+                return BadRequest("No image uploaded.");
+            }
+            var uploadPath = Path.Combine(_env.ContentRootPath, "uploads");
+            Directory.CreateDirectory(uploadPath); // Ensure the directory exists
 
-			return BadRequest(message);
-		}
-	}
+           //path to the location to save image (with the file name included) .ie : <solution>/uploads/<filename>.<ext>
+            var filePath = Path.Combine(uploadPath, Path.GetFileName(productImage.FileName));
+
+            // Save the image to the server
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await productImage.CopyToAsync(stream);
+            }
+
+            //Generate image path and create Product
+            request.ProductImage = productImage.FileName;
+
+            var (isCreated, message) = await _productServices.ModCreateProduct(request);
+            if (isCreated)
+                return Ok(message);
+
+            return BadRequest(message);
+        }
+    }
 }
